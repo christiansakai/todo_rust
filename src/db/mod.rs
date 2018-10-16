901794e9351c71cea8e4c322fd1bde2ac18f11ca
Christian Sakai <christianmsakai@gmail.com>
2018-10-16 18:33:38 -0400

    Got stuck

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,11 @@
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+
+use std::ops::Deref;
 
 pub mod schema;
 pub mod models;
@@ -12,4 +17,27 @@ pub fn init_pool(database_url: &str) -> PgPool {
 
     Pool::new(manager)
         .expect(&format!("Error connecting to {}", database_url))
+}
+
+struct DbConn(PooledConnection<ConnectionManager<PgConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let pool = request.guard::<State<PgPool>>()?;
+
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+        }
+    }
+}
+
+impl Deref for DbConn {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
--- src/db/models.rs
+++ src/db/models.rs
@@ -1,6 +1,34 @@
+use diesel;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+use super::schema::todos;
+use super::schema::todos::dsl;
+
+#[derive(Insertable)]
+#[table_name="todos"]
+pub struct NewTodo<'a> {
+    pub title: &'a str,
+    pub description: &'a str,
+}
+
 #[derive(Queryable)]
 pub struct Todo {
     pub id: i32,
     pub title: String,
     pub description: String,
+}
+
+impl<'a> NewTodo<'a> {
+    pub fn insert(new_todo: NewTodo, db_conn: &PgConnection) -> Todo {
+        diesel::insert_into(todos::table)
+            .values(&new_todo)
+            .get_result(db_conn)
+            .expect("Error saving new todo")
+    }
+}
+
+impl Todo {
+    pub fn all(db_conn: &PgConnection) -> Vec<Todo> {
+    }
 }
